3e3dd148f9d99af11de41e52e32e6aedd1807111
tanxiaoming <1147256541@qq.com>
2019-12-17 17:48:28 +0800

    新增项目

---
+++ serice-feign/src/main/java/com/xiaoming/ServiceFeignApp.java
@@ -0,0 +1,22 @@
+package com.xiaoming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+public class ServiceFeignApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ServiceFeignApp.class,args);
+        System.out.println( "Hello World! ServiceFeignApp" );
+    }
+}
