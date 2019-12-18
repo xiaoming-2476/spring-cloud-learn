4afacd65f4f99442f719151593236667e20be5b6
tanxiaoming <1147256541@qq.com>
2019-12-18 16:07:23 +0800

    没有成功的dashboard 和turbine

--- service-hello/src/main/java/com/xiaoming/config/RibbonConfig.java
+++ service-hello/src/main/java/com/xiaoming/config/RibbonConfig.java
@@ -1,5 +1,7 @@
 package com.xiaoming.config;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +14,15 @@ public class RibbonConfig {
     @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
